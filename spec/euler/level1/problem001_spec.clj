d31d99ada5600aa75d4f74cf3a8c1b5650ee729d
Jake Ogden <jakeogden@Jakes-Mac-mini.local>
2023-05-03 16:06:09 -0700

    solved problem 1

--- spec/euler/level1/problem001_spec.clj
+++ spec/euler/level1/problem001_spec.clj
@@ -7,9 +7,17 @@
 
 (describe "Euler Problem #1"
 
+  (it "knows if n is a multiple of 3 or 5"
+    (should= false (multiple-of-5-3? 8))
+    (should= true (multiple-of-5-3? 3))
+    (should= true (multiple-of-5-3? 5)))
+
+  (it "finds multiples less than n"
+    (should= [3 5 6 9] (multiples-less-than 10))
+    (should= [3 5 6 9 10 12 15] (multiples-less-than 16)))
+
   (it "Solves #1"
     (should= 23 (euler-1 10))
-	(should= -1 (euler-1 1000)))
-  )
+    (should= 233168 (euler-1 1000)))
 
-(run-specs)
+  )
--- src/euler/level1/problem001.clj
+++ src/euler/level1/problem001.clj
@@ -1,4 +1,11 @@
 (ns euler.level1.problem001)
 
+(defn multiple-of-5-3? [n]
+  (or (zero? (mod n 3))
+      (zero? (mod n 5))))
+
+(defn multiples-less-than [n]
+  (filter multiple-of-5-3? (rest (range n))))
+
 (defn euler-1 [n]
-  )
+  (apply + (multiples-less-than n)))
